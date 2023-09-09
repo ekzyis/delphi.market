08d85ef4777ee2de4f066355a150688da1e30d62
ekzyis <ekzyis@ekzyis.com>
2023-09-09 22:52:50 +0200

    Serve error pages

--- router.go
+++ router.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -22,4 +24,39 @@ func index(c echo.Context) error {
 
 func login(c echo.Context) error {
 	return c.Render(http.StatusOK, "login.html", map[string]string{"user": ""})
+}
+
+func serve500(c echo.Context) {
+	f, err := os.Open("public/500.html")
+	if err != nil {
+		c.Logger().Error(err)
+		return
+	}
+	err = c.Stream(500, "text/html", f)
+	if err != nil {
+		c.Logger().Error(err)
+		return
+	}
+}
+
+func httpErrorHandler(err error, c echo.Context) {
+	c.Logger().Error(err)
+	code := http.StatusInternalServerError
+	httpError, ok := err.(*echo.HTTPError)
+	if ok {
+		code = httpError.Code
+	}
+	filePath := fmt.Sprintf("public/%d.html", code)
+	f, err := os.Open(filePath)
+	if err != nil {
+		c.Logger().Error(err)
+		serve500(c)
+		return
+	}
+	err = c.Stream(code, "text/html", f)
+	if err != nil {
+		c.Logger().Error(err)
+		serve500(c)
+		return
+	}
 }
--- server.go
+++ server.go
@@ -49,6 +49,7 @@ func main() {
 		Format:           "${time_custom} ${method} ${uri} ${status}\n",
 		CustomTimeFormat: "2006-01-02 15:04:05.00000-0700",
 	}))
+	e.HTTPErrorHandler = httpErrorHandler
 	err := e.Start(":8080")
 	if err != http.ErrServerClosed {
 		log.Fatal(err)
