88db5f6ffb717e5f574a6d1d65b7f6bebc7b77a3
ekzyis <ekzyis@ekzyis.com>
2023-11-27 18:27:56 +0100

    Remove color for order description

--- vue/src/components/OrderRow.vue
+++ vue/src/components/OrderRow.vue
@@ -1,7 +1,7 @@
 <template>
   <tr>
     <td v-if="order.MarketId"><router-link :to="/market/ + order.MarketId">{{ order.MarketId }}</router-link></td>
-    <td :class="descClassName">{{ order.side }} {{ order.quantity }} {{ order.ShareDescription }} @ {{ order.price }} sats
+    <td>{{ order.side }} {{ order.quantity }} {{ order.ShareDescription }} @ {{ order.price }} sats
     </td>
     <td :title="order.CreatedAt" class="hidden-sm">{{ ago(new Date(order.CreatedAt)) }}</td>
     <td :class="'font-mono ' + statusClassName">{{ order.Status }}</td>
@@ -15,10 +15,6 @@ import ago from 's-ago'
 const props = defineProps(['order'])
 
 const order = ref(props.order)
-
-const descClassName = computed(() => {
-  return order.value.side === 'BUY' ? 'success' : 'error'
-})
 
 const statusClassName = computed(() => {
   const status = order.value.Status
