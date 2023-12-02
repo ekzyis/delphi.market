7fc63f44cdd936fe6c25918fe43eb9513ebabc93
ekzyis <ekzyis@ekzyis.com>
2023-12-02 02:27:41 +0100

    Fix order match link

--- vue/src/components/MarketOrders.vue
+++ vue/src/components/MarketOrders.vue
@@ -43,14 +43,14 @@ const click = (order) => {
     const stake = order.quantity * (100 - order.price)
     const certainty = (100 - order.price) / 100
     const share = order.ShareDescription === 'YES' ? 'NO' : 'YES'
-    router.push(`/market/${marketId}/form/buy?stake=${stake}&certainty=${certainty}&share=${share}`)
+    router.push(`/market/${marketId}/form?stake=${stake}&certainty=${certainty}&share=${share}&side=BUY`)
   }
   if (order.side === 'SELL') {
-    // match SELL YES with BUY YES and vice versa
+    // SELL YES -> BUY YES, SELL NO -> BUY NO
     const stake = order.quantity * order.price
     const certainty = order.price / 100
     const share = order.ShareDescription === 'YES' ? 'YES' : 'NO'
-    router.push(`/market/${marketId}/form/buy?stake=${stake}&certainty=${certainty}&share=${share}`)
+    router.push(`/market/${marketId}/form?stake=${stake}&certainty=${certainty}&share=${share}&side=BUY`)
   }
 }
 
